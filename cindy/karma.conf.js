d702c439d4eb8ddddd3c1c7ec9dbc68d89ccc177
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-06-29 13:59:34 +0300

    create the cindy app

--- cindy/karma.conf.js
+++ cindy/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/CindyFront'),
+      dir: require('path').join(__dirname, './coverage/cindy'),
       subdir: '.',
       reporters: [
         { type: 'html' },
