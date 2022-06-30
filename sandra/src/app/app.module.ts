508e4bc4cfbae19725cc6706267b14b31a9aee4d
sandra kinoti <sandra.kinoti@student.moringaschool.com>
2022-06-30 10:33:46 +0300

    add cart service

--- sandra/src/app/app.module.ts
+++ sandra/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ProductComponent } from './component/product/product.component';
     CartComponent,
     ProductComponent
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule
--- sandra/src/app/service/cart.service.spec.ts
+++ sandra/src/app/service/cart.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ApiService } from './api.service';
+import { CartService } from './cart.service';
 
-describe('ApiService', () => {
-  let service: ApiService;
+describe('CartService', () => {
+  let service: CartService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(ApiService);
+    service = TestBed.inject(CartService);
   });
 
   it('should be created', () => {
