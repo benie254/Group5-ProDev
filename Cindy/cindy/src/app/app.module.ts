8ddd662238e82fa9e9d95dbab29a1ed332dccfef
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-07-05 16:08:38 +0300

    Import the compoents

--- Cindy/cindy/src/app/app.module.ts
+++ Cindy/cindy/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { HeaderComponent } from './header/header.component';
 import { FormsModule } from '@angular/forms';
+import { UsersComponent } from './component/users/users.component';
+import { UserdetailComponent } from './component/userdetail/userdetail.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, UsersComponent, UserdetailComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
