905bc4391af728024c0c57dfc1d9098e3040766a
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-07-05 16:09:08 +0300

    Import and update the post and get requests

--- Cindy/cindy/src/app/app.component.ts
+++ Cindy/cindy/src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Book } from './book-class/book';
-import { PostService } from './services/post.service';
 import { HttpClient } from '@angular/common/http';
-
+import { UserService } from './services/user.service';
+import { Root } from './interface/root';
+import { UsersDataService } from './services/users-data.service';
+import { BookServiceService } from './services/book-service.service';
 
 @Component({
   selector: 'app-root',
@@ -10,34 +11,64 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  posts: any;
-  book: Book;
-  userData: any;
+  title = 'Posts';
+  users: any;
+  private user: Root = {
+    title: '',
+    author: '',
+    description: '',
+    publisher: '',
+  };
+
+  constructor(
+    private http: HttpClient,
+    private userService: UserService,
+    private UsersData: UsersDataService,
+    private bookService: BookServiceService
   
-
-  constructor(private service: PostService, private http: HttpClient) {}
-
-  ngOnInit() {
-    this.service.getPosts().subscribe((response: any) => {
-      this.posts = response;
+  ) {
+    this.UsersData.users().subscribe((data) => {
+      this.users = data;
     });
-    interface ApiResponse {
-      author: string;
-      description: string;
-    }
-    this.http
-      .get<Book>('https://gfive.herokuapp.com/api/books/')
-      .subscribe((data: { author: string; description: string; }) => {
-        console.log('book dret', this.book);
-        
-        // Succesful API request
-        this.book = new Book(data.author, data.description);
-      });
   }
-  getUserFormData(data:any){
-      console.warn(data)
-      this.userData.saveUser(data).subscribe((result: any)=>{
-        console.warn
-      })
+  getUserFormData(data: any) {
+    console.warn(data);
+    this.UsersData.saveUser(data).subscribe((data) => {
+      console.warn(data);
+    });
+  }
+  submitBookForm(book_data: any) {
+    console.warn(book_data);
+    this.bookService.addBook(book_data).subscribe((result) => {
+      console.warn(result);
+      // this.addBook=result
+    });
+  }
+
+  ngOnInit(): void {
+    // this.onGetUser();
+    this.onGetUsers();
+    // this.onCreateUser();
+  }
+  onGetUsers(): void {
+    this.userService.getUsers().subscribe(
+      (response) => console.table(response),
+      (error: any) => console.log(error),
+      () => console.log('Done getting users')
+    );
+  }
+  onGetUser(): void {
+    this.userService.getUser().subscribe(
+      (response) => console.log(response),
+      (error: any) => console.log(error),
+      () => console.log('Done getting user')
+    );
+  }
+  onCreateUser(): void {
+    this.userService.createUser(this.user).subscribe(
+      (response) => console.log(response),
+      (error: any) => console.log(error),
+      () => console.log('Done creating user')
+    );
   }
 }
