109f0d2bd61e1cf0c7d92efd8eca5ac3f87fd8b5
Benie <davinci.monalissa@gmail.com>
2022-07-10 05:56:30 +0300

    initialize structure

--- benson/BensonFront/karma.conf.js
+++ benson/BensonFront/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/BensonFront'),
+      dir: require('path').join(__dirname, './coverage/benson-front'),
       subdir: '.',
       reporters: [
         { type: 'html' },
