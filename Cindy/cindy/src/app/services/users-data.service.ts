31df7452ef3ba0edc7cb8428fb04bc22fd6e6201
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-07-11 15:37:53 +0300

    Dev:get details of one user

--- Cindy/cindy/src/app/services/users-data.service.ts
+++ Cindy/cindy/src/app/services/users-data.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Root } from '../interface/root';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UsersDataService {
-  url = 'https://gfive.herokuapp.com/api/books/<int:pk>/';
+  url = 'https://gfive.herokuapp.com/api/books/';
 
   constructor(private http: HttpClient) {}
   users() {
@@ -13,5 +15,8 @@ export class UsersDataService {
   }
   saveUser(data: any) {
     return this.http.post(this.url, data);
+  }
+  getUser(id: number = 1): Observable<Root> {
+    return this.http.get<Root>(`${this.url}`);
   }
 }
