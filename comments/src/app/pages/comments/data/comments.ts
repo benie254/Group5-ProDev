8d04e25d55881ac478623815ba55d9b4ab3409a8
sandra kinoti <sandra.kinoti@student.moringaschool.com>
2022-07-13 14:22:27 +0300

    save changes

--- comments/src/app/pages/comments/data/comments.ts
+++ comments/src/app/pages/comments/data/comments.ts
@@ -19,7 +19,7 @@ export const TempData: {currentUser: IUser, comments: IComment[]} = {
     {
       id: 1,
       content:
-        "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
+      "Loved this book. Wish i could read it again for the first time",
       createdAt: getDateTime(1000 * 3600 * 24 * 35),
       score: 12,
       user: {
@@ -34,7 +34,7 @@ export const TempData: {currentUser: IUser, comments: IComment[]} = {
     {
       id: 2,
       content:
-        "Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
+      "Loved the authors style of writing very captivating",
       createdAt: getDateTime(1000 * 3600 * 24 * 16),
       score: 5,
       user: {
@@ -48,7 +48,7 @@ export const TempData: {currentUser: IUser, comments: IComment[]} = {
         {
           id: 3,
           content:
-            "If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
+          "Would recommend this book. Good read",
           createdAt: getDateTime(1000 * 3600 * 24 * 8),
           score: 4,
           replyingTo: 'maxblagun',
@@ -64,7 +64,7 @@ export const TempData: {currentUser: IUser, comments: IComment[]} = {
         {
           id: 4,
           content:
-            "I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
+          "5 stars",
           createdAt: getDateTime(1000 * 3600 * 24 * 2.3),
           score: 2,
           replyingTo: 'ramsesmiron',
