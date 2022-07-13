1502e1fcfa7c5c34f094222ddf93072251a1f3e8
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-07-13 15:41:27 +0300

    Add the user profile route

--- Cindy/cindy/src/app/app-routing.module.ts
+++ Cindy/cindy/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { UserdetailComponent } from './component/userdetail/userdetail.component';
 import { UsersComponent } from './component/users/users.component';
 import { AddPostComponent } from './component/add-post/add-post.component';
+import { UserProfileComponent } from './component/user-profile/user-profile.component';
 
 const routes: Routes = [
   { path: 'users', component: UsersComponent },
   { path: 'user/:id', component: UserdetailComponent },
   { path: 'addBook', component: AddPostComponent},
+  { path: 'userprofile/:id', component: UserProfileComponent},
   { path: '**', redirectTo:'users' },
 ];
 
