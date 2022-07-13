551773bff4d25dc9ac37482ac90b93c56e53dd7a
Benie <davinci.monalissa@gmail.com>
2022-07-13 03:35:57 +0300

    update branch

--- lee/django-auth/auth/settings.py
+++ lee/django-auth/auth/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,8 +82,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'auth',
-        'USER': 'postgres',
-        'PASSWORD': 'lee123'
+        'USER': 'benie',
+        'PASSWORD': '12345'
     }
 }
 
