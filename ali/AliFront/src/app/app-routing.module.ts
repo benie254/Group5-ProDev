bc090f2d9e6d0b8ec59f9ff8daee64150c9c8d2d
Ali-Mohamud <93277692+Ali-Mohamud@users.noreply.github.com>
2022-07-14 20:34:41 +0300

    update ui

--- ali/AliFront/src/app/app-routing.module.ts
+++ ali/AliFront/src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookslistComponent } from './bookslist/bookslist.component';
+import { CartComponent } from './cart/cart.component';
+// import { profile } from 'console';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProductsComponent } from './products/products.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ReaderprofileComponent } from './readerprofile/readerprofile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'' ,component:HomeComponent},
+  {path:'login', component:LoginComponent},
+  {path:'profile', component:ProfileComponent},
+  {path:'products', component:ProductsComponent},
+  {path:'readerprofile', component:ReaderprofileComponent},
+  {path:'cart', component:CartComponent},
+  {path:'bookslist', component:BookslistComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+ }
