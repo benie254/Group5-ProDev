b4109bce0d5a381e4464528d4086807dc2e759e9
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-07-14 14:36:16 +0300

    remove redundant code

--- Cindy/cindy/src/app/services/users-data.service.ts
+++ Cindy/cindy/src/app/services/users-data.service.ts
@@ -11,16 +11,12 @@ export class UsersDataService {
   url = 'https://gfive.herokuapp.com/api/books/';
  
 
-  // let /
-
   constructor(private http: HttpClient) {}
 
   users() {
     return this.http.get(this.url);
   }
-httpHeeaders=new HttpHeaders({'content-Type':'application/json'})
   
-
   getUser(id: number = 1): Observable<Root> {
     return this.http.get<Root>(`${this.url + id}`);
   }
