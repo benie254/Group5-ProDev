f7e942caf4de921e2c1bb8859c358b670237d523
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-07-14 14:37:27 +0300

    Dev:add user properties

--- Cindy/cindy/src/app/interface/user.ts
+++ Cindy/cindy/src/app/interface/user.ts
@@ -1,5 +1,9 @@
 export interface User {
-    id?: number;
-    name: string;
-    email: string;
+  id?: number;
+  user: any;
+  profile_picture: string;
+  bio: string;
+  name: string;
+  phone_number: string;
+  email: string;
 }
