29e79561ae9366df5b7027f96b49b5359decaaa0
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-07-14 10:42:45 +0300

    Change and remove redundant code in the book service

--- Cindy/cindy/src/app/services/book-service.service.ts
+++ Cindy/cindy/src/app/services/book-service.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { Book } from '../classes/books/book';
 @Injectable({
   providedIn: 'root',
 })
 export class BookServiceService {
-  url = 'https://gfive.herokuapp.com/api/books/';
-  
-  constructor(private http:HttpClient) {}
+  book: Book;
+  // url = 'https://gfive.herokuapp.com/api/books/';
+  readonly url = 'http://127.0.0.1:8000/api/books/';
+
+  constructor(private http: HttpClient) {}
 
   addBook(book_data: any) {
+    return this.http.post(this.url, book_data);
+  }
+  saveUser(book_data: any) {
+    // return this.http.post(this.url, data);
     return this.http.post(this.url, book_data);
   }
 }
