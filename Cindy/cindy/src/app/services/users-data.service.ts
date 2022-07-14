43bde2230901dd37e5529a17f9c684b120cf7c92
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-07-14 10:41:35 +0300

    Remove redundant code

--- Cindy/cindy/src/app/services/users-data.service.ts
+++ Cindy/cindy/src/app/services/users-data.service.ts
@@ -2,15 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, Observable, retry } from 'rxjs';
 import { Root } from '../interface/root';
-import { Book } from '../classes/books/book';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class UsersDataService {
-  book: Book;
-  // url = 'https://gfive.herokuapp.com/api/books/';
- readonly url = "http://127.0.0.1:8000/api/books/";
+  url = 'https://gfive.herokuapp.com/api/books/';
+ 
 
   // let /
 
@@ -21,35 +20,6 @@ export class UsersDataService {
   }
 httpHeeaders=new HttpHeaders({'content-Type':'application/json'})
   
-saveUser(book_data: any) {
-    // return this.http.post(this.url, data);
-    return this.http.post(this.url, book_data)
-    
-  }
-
-  // getUsers = async (data) => {
-  //   // const location = window.location.hostname;
-  //   const settings = {
-  //     method: 'POST',
-  //     headers: {
-  //       Accept: 'application/json',
-  //       'Content-Type': 'application/json',
-  //     },
-  //   };
-  //   try {
-  //     const fetchResponse = await fetch(
-  //       `http://127.0.0.1:8000/api/books/` data,
-  //       settings
-  //     );
-  //     const data = await fetchResponse.json();
-  //     console.log(data)
-  //     return data;
-  //   } catch (e) {
-  //     console.log();
-
-  //     return e;
-  //   }
-  // };
 
   getUser(id: number = 1): Observable<Root> {
     return this.http.get<Root>(`${this.url + id}`);
